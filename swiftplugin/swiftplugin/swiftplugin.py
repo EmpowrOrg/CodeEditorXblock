3f15b339c0c66e1c67efc709f25c772f76f5d505
farazahmad759 <farazahmad759@gmail.com>
2022-10-24 22:38:50 +0500

    fixed bootstrap css interference

--- swiftplugin/swiftplugin/swiftplugin.py
+++ swiftplugin/swiftplugin/swiftplugin.py
@@ -118,7 +118,7 @@ class SwiftPluginXBlock(
         frag.add_javascript_url("https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js")
         frag.add_css(self.resource_string("static/css/swiftplugin.css"))
         frag.add_javascript(self.resource_string("static/js/src/swiftplugin.js"))
-        frag.add_css_url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css")
+        frag.add_css(self.resource_string("static/css/bootstrap.min.css"))
         frag.add_javascript_url(
             "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js")
         frag.add_css_url(
