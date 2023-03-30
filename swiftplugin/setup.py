ec17b89eea62fd26ecff0b5855c4e8a68e2c6670
adevezin <adrianldevezin@mailfence.com>
2023-03-30 04:42:51 -0400

    Update license to AGPL v3

--- swiftplugin/setup.py
+++ swiftplugin/setup.py
@@ -26,7 +26,7 @@ setup(
     name='swiftplugin-xblock',
     version='0.1',
     description='swiftplugin XBlock',   # TODO: write a better description.
-    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    license='AGPL v3',
     packages=[
         'swiftplugin',
     ],
